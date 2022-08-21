9d1efd1723d72a4c341fb6d39e6be413a51a2796
Branimirius <73712742+Branimirius@users.noreply.github.com>
2022-08-21 15:12:18 +0200

    angular frontent and spring initialization

---
+++ advertisements-backend/src/main/java/com/example/advertisementsbackend/AdvertisementsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.advertisementsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertisementsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertisementsBackendApplication.class, args);
+	}
+
+}
